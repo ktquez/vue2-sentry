60c44ef93bdaeef8fe464e440575538e20e79190
Alan Albuquerque (ktquez) <ktquez@gmail.com>
2017-10-18 01:10:13 -0300

    Fix configuration of sentry on moment of installation

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import RavenVue from 'raven-js/plugins/vue'
 
 export default function VueSentry (Vue, options = {}) {
   Raven
-      .config(`${options.protocol || 'https'}://${options.key}@sentry.io/${options.project}`, ...options.config)
+      .config(`${options.protocol || 'https'}://${options.key}@sentry.io/${options.project}`, {
+        ...options.config
+      })
       .addPlugin(RavenVue, Vue)
       .install()  
   
