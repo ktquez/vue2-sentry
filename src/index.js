4b098ad5a4d73fd639301a740899ef3dc2be11f5
Alan Albuquerque (ktquez) <ktquez@gmail.com>
2017-10-18 23:01:08 -0300

    Add option for enable in specific environment

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import Raven from 'raven-js'
 import RavenVue from 'raven-js/plugins/vue'
 
-export default function VueSentry (Vue, options = {}) {
+export default function VueSentry (Vue, options) {
+
+  const _options = {
+    enable: true,
+    config: {}
+  }
+
+  if (options) {
+    Vue.util.extend(_options, options)
+  }
+
+  if (!options.enable) return
+
   Raven
       .config(`${options.protocol || 'https'}://${options.key}@sentry.io/${options.project}`, {
         ...options.config
